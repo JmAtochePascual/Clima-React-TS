05bb0360859314d309e8d4bf7f88a22a6ab561b3
JeanCarlos <jmatochepascual@gmail.com>
2024-10-21 18:12:57 -0500

    feat: :sparkles: Agrega estilos globales

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import styles from './app.module.css'
 
 function App() {
 
   return (
     <>
-      <h1>Buscador clima</h1>
+      <h1 className={styles.title}>Buscador clima</h1>
+
+      <div className={styles.container}>
+        <div className="1">1</div>
+        <div className="2">2</div>
+      </div>
     </>
   )
 }
